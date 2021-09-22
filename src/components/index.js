89405e50361bbf87255f4670bf856e18bc3908cc
Milos Avramovic <avramovic.m@oilpc.ru>
2021-09-22 16:38:29 +0200

    First commit. - Added necessaries imports. - Layout is finished.

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Navbar} from "../components/Layout/Navbar/Navbar";
+export {default as Homepage} from "../components/Layout/Homepage/Homepage";
+export {default as Cryptocurrencies} from "../components/Layout/Cryptocurrencies/Cryptocurrencies";
+export {default as CryptoDetails} from "../components/Layout/CryptoDetails/CryptoDetails";
+export {default as Exchanges} from "../components/Layout/Exchanges/Exchanges";
+export {default as News} from "../components/Layout/News/News";
